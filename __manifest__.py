0038da793eeaa83d3c694459b4d357cde2f349fc
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-04-05 11:46:01 +0200

    Exercise 1 Report Build PDF

--- __manifest__.py
+++ __manifest__.py
@@ -11,13 +11,16 @@
     'website': '',
     'depends': [],
     'data': [
-        #'models/estate_property.py'
+        # 'models/estate_property.py'
         'security/ir.model.access.csv',
         'views/estate_property.xml',
         'views/estate_property_type.xml',
         'views/estate_property_tag.xml',
         'views/estate_property_offer.xml',
         'views/inherited_model.xml',
+        'report/estate_property_templates.xml',
+        'report/estate_property_reports.xml',
+
     ],
     'demo': [],
     'css': [''],
